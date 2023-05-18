83b47c6155f7ceaf45d4df230283d67bd5531788
Alexandru-Costin Gheorghiu <57139895+Powerinno@users.noreply.github.com>
2023-05-18 15:34:43 +0300

    Update action.ps1

--- action.ps1
+++ action.ps1
@@ -2,4 +2,6 @@ param(
     $EVENTDATA
 )
 
-Write-Output($EVENTDATA.EventProperties.Message)
+$split_out = $EVENTDATA.EventProperties.Message.Split(" ")
+Write-Output($split[0])
+Write-Output($split[1])
